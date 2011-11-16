2ed11ebfec7bb08f4f06ed81616fabe411a8fba8
Daniel Pakuschewski <contato@danielpk.com.br>
2011-11-16 17:20:11 -0200

    Fixed alot of bugs

--- models/behaviors/soft_validation.php
+++ models/behaviors/soft_validation.php
@@ -1,69 +1,129 @@
 <?php
 class SoftValidationBehavior extends ModelBehavior {
 /**
- * Hold the settings of models.
- */
+  * Hold the settings of models.
+  */
 	public $settings = array();
 /**
- * Hold the softValidate rules extracted from model.
- * Eg: array('ModelName' => array( //rules extracted ));
- */
-	protected $_softValidate = array();
+  * Hold the softValidate rules extracted from model.
+  * Eg.: array('ModelName' => array( //rules extracted ));
+  */
+	protected $_softValidateFields = array();
 /**
- * Hold default settings.
- */
+  * Hold default settings.
+  */
 	protected $_defaultSettings = array();
 /**
- * Hold the model isntance.
- */
-	private $__model;
+  * Hold the softValidate status of model.
+  * Eg.: array('ModelName' => true);
+  */
+	protected $_softValidateStatus = array();
+/**
+  * Hold the model isntance.
+  */
+	private $__Model;
 
 /**
- * Setup
- */
-	public function setup(&$model, $config = array()) {
-		$this->__model =& $model;
-		$this->_softValidate[$model->alias] = $this->_parseSoftValidates();
+  * Setup
+  */
+	public function setup(&$Model, $config = array()) {
+		$this->__Model =& $Model;
+		$this->_softValidateFields[$Model->alias] = $this->_parseSoftValidates();
+	}
+/**
+  * beforeSave callback
+  */
+	public function beforeSave(&$Model){
+		$this->_softValidate();
+		return $this->_triggerCallback();
 	}
 
-	public function beforeSave(&$model){
-		$model->validate = Set::merge($model->validate, $this->_softValidate[$model->alias]);
-		$validates = $model->validates();
-		$model->validationErrors = array();
-
+	protected function _softValidate(){
+		$this->__Model->validate = Set::merge($this->__Model->validate, $this->_softValidateFields[$this->__Model->alias]);
+		$validates = $this->__Model->validates();
+		$this->__Model->validationErrors = array();
 		if($validates){
-			return $model->beforeSaveSoftValidationSuccess();
+			$this->_softValidationUpdateStatus(true);
 		}else{
-			return $model->beforeSaveSoftValidationFail();
+			$this->_softValidationUpdateStatus(false);
 		}
-		
-		return true;
 	}
 
+	protected function _triggerCallback(){
+		if($this->getSoftValidationStatus($this->__Model)){
+			return $this->__Model->beforeSaveSoftValidationSuccess();
+		}else{
+			return $this->__Model->beforeSaveSoftValidationFail();
+		}
+	}
 /**
- * Return the soft validation and remove them from
- * model validate to prevent validations on soft.
- */
+  * Get the Soft Validation status.
+  *
+  * @return boolean
+  */
+	public function getSoftValidationStatus(&$Model, $data = null){
+		if(!is_null($data)){
+			$this->__Model = $Model;
+			$this->__Model->set($data);
+			$this->_softValidate();
+		}
+		debug($this->_softValidateStatus);
+		if(isset($this->_softValidateStatus[$Model->alias])){
+			return $this->_softValidateStatus[$Model->alias];
+		}
+		return false;
+	}
+/**
+  * Return the fields soft validated.
+  * 
+  * @return mixed 
+  * 		array with fields name or false if model dont have soft validates.
+  */
+	public function getSoftValidationFields(&$Model){
+		if(isset($this->_softValidateFields[$Model->alias])){
+			return array_keys($this->_softValidateFields[$Model->alias]);
+		}
+		return false;
+	}
+/**
+  * Return the soft validation and remove fields from
+  * model validate to prevent validations on soft fields.
+  *
+  * @return array of soft validation rules
+  */
 	protected function _parseSoftValidates(){
 		$softValidate = array();
-		foreach ($this->__model->validate as $field => $rules){
+		foreach ($this->__Model->validate as $field => $rules){
 			foreach($rules as $rule){
 				if(!is_array($rule)){
 					continue;
 				}
 				if(isset($rule['soft']) && $rule['soft'] == true){
 					$softValidate[$field] = $rules;
-					unset($this->__model->validate[$field]);
+					unset($this->__Model->validate[$field]);
 				}
 			}
 		}
 		return $softValidate;
 	}
+/**
+  * Set the Soft Validation status.
+  *
+  * @return boolean
+  */
+	protected function _softValidationUpdateStatus($status){
+		$this->_softValidateStatus[$this->__Model->alias] = $status;
+		return true;
+	}
 
-	/**
-	 * Callbacks API.
-	 */
+/**
+  * Callbacks API.
+  *
+  * Called when all fields are filled.
+  */
 	public function beforeSaveSoftValidationSuccess(){	return true; }
-
+/**
+  * Called when soft fields is not filled.
+  */
 	public function beforeSaveSoftValidationFail(){ return true; }
 }
